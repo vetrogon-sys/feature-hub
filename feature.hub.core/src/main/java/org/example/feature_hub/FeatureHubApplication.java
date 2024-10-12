e1cdfa6852c3cf83888405c3883ae126146301c5
Kostya <kostyabogdanov338@gmail.com>
2024-10-12 17:47:33 +0300

    initial commit

---
+++ feature.hub.core/src/main/java/org/example/feature_hub/FeatureHubApplication.java
@@ -0,0 +1,17 @@
+package org.example.feature_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class FeatureHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureHubApplication.class, args);
+    }
+
+}
---
+++ feature.hub.core/src/test/java/org/example/feature_hub/FeatureHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.feature_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
